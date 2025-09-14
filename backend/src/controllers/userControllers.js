4abc7bc63bc1445363d97ee5359a5942cf8c4420
Muh Irfan <muh.irpan11345@gmail.ocm>
2025-09-14 22:10:13 +0800

    Add JWT_SECRET to .env and update userControllers.js to use email and nip for logn

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,14 +1,37 @@
-import { prisma } from "../configs/utils";
+import { prisma } from "../configs/utils.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 const loginUser = async (req, res) => {
   try {
-    const { nama, nip } = req.body;
-    if (!nama || !nip) {
-      return res.status(400).json({ message: "Nama and NIP are required" });
+    const { email, nip } = req.body;
+    if (!email || !nip) {
+      return res.json({ message: "Email and NIP are required" });
     }
 
-    const user = await prisma.user.findUnique({});
-  } catch (error) {}
+    const user = await prisma.user.findUnique({ nip });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = nip === user.nip;
+
+    if (isMatch) {
+      const token = createToken(user.id);
+      res.json({ success: true, message: "Login successful", token });
+    } else {
+      res.json({ success: false, message: "Invalid NIP" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export { loginUser };
