9ce235f8b6643da3b3698bad40d190f55bf9aee0
Muh Irfan <muh.irpan11345@gmail.ocm>
2025-09-14 23:22:57 +0800

    Delete email login

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -7,35 +7,24 @@ const createToken = (id) => {
 
 const loginUser = async (req, res) => {
   try {
-    const { email, nip } = req.body;
-    if (!email || !nip) {
-      return res.json({ message: "Email and NIP are required" });
+    const { nip } = req.body;
+    if (!nip) {
+      return res.json({ message: "NIP are required" });
     }
 
-    if (!validator.isEmail(email)) {
-      return res.json({ message: "Invalid email format" });
-    }
-
-    if (nip.length != 18) {
-      return res.json({ message: "NIP must be 18 characters long" });
-    }
-
-    const user = await prisma.user.findUnique({ nip });
+    const user = await prisma.user.findUnique({
+      where: { nip },
+    });
 
     if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
+      return res.status(404).json({
+        success: false,
+        message: "Invalid NIP",
+      });
     }
 
-    const isMatch = nip === user.nip;
-
-    if (isMatch) {
-      const token = createToken(user.id);
-      res.json({ success: true, message: "Login successful", token });
-    } else {
-      res.json({ success: false, message: "Invalid NIP" });
-    }
+    const token = createToken(user.id);
+    res.json({ success: true, message: "Login successful", token });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
